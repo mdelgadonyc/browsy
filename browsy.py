58944903bce01d0563f2fd99229364d6297f82a8
Moises Delgado <mdelgado.f.delgado@gmail.com>
2023-05-16 12:41:19 -0400

    Removes demo link used in testing

--- browsy.py
+++ browsy.py
@@ -4,10 +4,10 @@ import logging
 from playwright.sync_api import Playwright, sync_playwright, expect
 from pathlib import Path
 import bwfun
-import sites 
+import sites
+
 
 def run(playwright: Playwright, dictionary) -> None:
-
     # Read dictionary from file
     with open(dictionary) as file:
         data = file.read()
@@ -20,21 +20,20 @@ def run(playwright: Playwright, dictionary) -> None:
     logging.debug(f'chromium_dir is: {chromium_dir}')
     browser = playwright.chromium.launch_persistent_context(chromium_dir, headless=False)
 
+    # Playwright "Hello World!""
     page = browser.new_page()
-    page.goto( js["$HOME_PAGE"] )
+    page.goto(js["$HOME_PAGE"])
     page.get_by_role("combobox", name="Search").click()
     page.get_by_role("combobox", name="Search").fill("Hello world!")
     page.get_by_role("combobox", name="Search").press("Enter")
 
     # uncomment line below to skip to jobsite for debugging purposes
-    # sites.jobsite(page, js)
+    # sites.jobapply(page, js)
 
     while (1):
         cmd = input("--> ")
         if cmd == 'exit':
             exit()
-        if cmd == 'youtube':
-            page.goto("https://www.youtube.com/")
         if cmd == 'simple':
             page.goto("https://phet-dev.colorado.edu/html/build-an-atom/0.0.0-3/simple-text-only-test-page.html")
         if cmd == 'bwlogin':
@@ -43,8 +42,11 @@ def run(playwright: Playwright, dictionary) -> None:
             print(page.content())
         if cmd == js["JOBSITE"]:
             sites.jobsite(page, js)
+            # sites.jobapply(page, js)
+
         if cmd == js["WORK"]["WORKSITE"]:
             sites.worksite(page, js)
+
 
 prog_name = sys.argv[0]
 
