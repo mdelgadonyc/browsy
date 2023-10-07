aa1d8a51f08f72f3d044272a779d7dd15dfc72d7
mdelgadonyc <moises.f.delgado@gmail.com>
2023-10-07 15:27:16 -0400

    Removes all the site specific code

--- browsy.py
+++ browsy.py
@@ -3,7 +3,6 @@ import sys
 import logging
 from playwright.sync_api import Playwright, sync_playwright, expect
 from pathlib import Path
-import sites
 
 
 def run(playwright: Playwright, dictionary) -> None:
@@ -19,17 +18,12 @@ def run(playwright: Playwright, dictionary) -> None:
     js = json.loads(data)
 
     logging.debug(f'Data from file: {js}')
-    logging.debug(f'job site name: {js["JOBSEARCH"]}')
 
     chromium_dir = Path.home() / '.config/chromium'
     logging.debug(f'chromium_dir is: {chromium_dir}')
     browser = playwright.chromium.launch_persistent_context(chromium_dir, headless=False)
 
     page = browser.new_page()
-
-    # uncomment either line below to skip to jobsearch or worksite for debugging purposes
-    # sites.jobsearch(page, js)
-    # sites.worksite(page, js, browser)
 
     # TODO: Add "Browsy" ASCII logo
 
@@ -41,10 +35,6 @@ def run(playwright: Playwright, dictionary) -> None:
             page.goto("https://phet-dev.colorado.edu/html/build-an-atom/0.0.0-3/simple-text-only-test-page.html")
         if cmd == 'dump':
             print(page.content())
-        if cmd == js["JOBSEARCH"]:
-            sites.jobsearch(page, js, browser)
-        if cmd == js["WORK"]["WORKSITE"]:
-            sites.worksite(page, js, browser)
 
 
 prog_name = sys.argv[0]
